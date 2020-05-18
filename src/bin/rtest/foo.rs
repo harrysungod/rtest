94f3fc134024637e04662fa8a852088562d176cb
Harry Sungod <harrysungod@gmail.com>
2020-05-18 15:49:33 -0700

    fix 1

--- src/bin/rtest/foo.rs
+++ src/bin/rtest/foo.rs
@@ -1,47 +1,43 @@
+use reqwest;
+use serde::{Deserialize, Serialize};
+use std::error;
 
-mod foo {
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Foo {
+    pub name: String,
+    pub foo_id: u32,
+    pub pid: u32,
+}
 
-    use reqwest;
-    use serde::{Deserialize, Serialize};
-    use std::error;
+#[derive(Debug, Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+struct FooListingResponse {
+    total: u32,
+    page_size: u32,
+    page: u32,
+    list: Vec<Foo>,
+}
 
-    #[derive(Debug, Serialize, Deserialize)]
-    #[serde(rename_all = "camelCase")]
-    pub struct Foo {
-        pub name: String,
-        pub foo_id: u32,
-        pub  pid: u32,
-    }
+#[derive(Debug)]
+pub struct FooConnection {
+    http_client: reqwest::blocking::Client,
+    access_token: String,
+    api_host: String,
+}
 
-    #[derive(Debug, Serialize, Deserialize)]
-    #[serde(rename_all = "camelCase")]
-    struct FooListingResponse {
-        total: u32,
-        page_size: u32,
-        page: u32,
-        list: Vec<Foo>,
-    }
-
-    #[derive(Debug)]
-    pub struct FooConnection {
-        http_client: reqwest::blocking::Client,
-        access_token: String,
-        api_host: String,
-    }
-
-    impl FooConnection {
-        pub fn new(auth_token: String, ims_env: String) -> FooConnection {
-            FooConnection {
-                http_client: reqwest::blocking::Client::new(),
-                api_host: String::from("https://api.example.com"),
-                access_token: auth_token,
-            }
+impl FooConnection {
+    pub fn new(auth_token: String, ims_env: String) -> FooConnection {
+        FooConnection {
+            http_client: reqwest::blocking::Client::new(),
+            api_host: String::from("https://api.example.com"),
+            access_token: auth_token,
         }
+    }
 
-        pub fn get_data(&mut self) -> Result<Vec<Foo>, Box<dyn error::Error>> {
-            let mut data_sources: Vec<Foo> = Vec::new();
+    pub fn get_data(&mut self) -> Result<Vec<Foo>, Box<dyn error::Error>> {
+        let mut data_sources: Vec<Foo> = Vec::new();
 
-            Ok(data_sources)
-        }
+        Ok(data_sources)
     }
 }
--- src/bin/rtest/main.rs
+++ src/bin/rtest/main.rs
@@ -3,7 +3,7 @@ mod foo;
 
 fn main() {
 
-    let mut _foo = foo::FooConnection::new("", "");
+    let mut _foo = foo::FooConnection::new(String::from(""), String::from(""));
     let data_sources = _foo.get_data().expect("Fetching data failed");
 
     println!("{:?}", data_sources);
