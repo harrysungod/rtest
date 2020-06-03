5df2a4779d278671ace7e9c7be9ada2107bcdfcb
Harry Sungod <harrysungod@gmail.com>
2020-06-03 10:40:22 -0700

    Larger example from deno_core crate

---
+++ src/bin/rtest/main_old.rs
@@ -0,0 +1,28 @@
+use deno_core::CoreIsolate;
+use deno_core::StartupData;
+use tokio;
+
+fn main() {
+    let startup_data = StartupData::Script(deno_core::Script {
+        source: "throw \"test1\";",
+        filename: "helloworld.js",
+    });
+
+    let mut core_isolate = CoreIsolate::new(startup_data, false);
+
+    /*
+    let result = core_isolate
+        .execute("something.js", "throw \"test2\";")
+        .expect("Execute failed");
+    */
+
+    let mut runtime = tokio::runtime::Builder::new()
+        .basic_scheduler()
+        .enable_all()
+        .build()
+        .unwrap();
+
+    runtime
+        .block_on(core_isolate)
+        .expect("unexpected isolate error");
+}
